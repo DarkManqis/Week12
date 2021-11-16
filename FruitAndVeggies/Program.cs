f4b2e130124242825a97f2ca2c3934742a113f64
Opilane TTHK <opilane@tthk.ee>
2021-11-16 16:31:47 +0200

    file

---
+++ FruitAndVeggies/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace FruitAndVeggies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //read data from fruit.txt
+            string fruitPath = @"C:\Users\opilane\Samples\fruit.txt";
+            string[] veggieData = File.ReadAllLines(fruitPath);
+
+            //read data from veggie.txt
+            string veggiePath = @"C:\Users\opilane\Samples\veggie.txt";
+            string[] fruitData = File.ReadAllLines(veggiePath);
+
+            File.WriteAllLines(fruitPath, fruitData);
+            File.WriteAllLines(veggiePath, veggieData);
+
+        }
+    }
+}
